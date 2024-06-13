26d94e9429a495fabe13798af12958344b4d50ad
Clover <2621869236@qq.com>
2024-06-13 17:22:04 +0800

    feat: datepicker 组件页脚 bug 示例

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { DatePicker, InputNumber } from 'ant-design-vue'
+</script>
+
+<template>
+  <div>
+    <p>1. 点击 DatePicker</p>
+    <p>2. 点击页脚中的 InputNumber 并随便输入点内容</p>
+    <p>3. 如果你点击 InputNumber 的 Control, 你会发现 DatePicker 立刻关闭</p>
+
+    <DatePicker>
+      <template #renderExtraFooter>
+        <InputNumber style="width: 100%" placeholder="输入点内容" />
+      </template>
+    </DatePicker>
+  </div>
+</template>
